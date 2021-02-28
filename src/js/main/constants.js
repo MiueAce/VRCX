e67c3fdc5648ae6abe366c069a62ff8a61403996
pypy <git@pypy.moe>
2021-02-28 20:26:55 +0900

    process constants

--- src/js/main/constants.js
+++ src/js/main/constants.js
@@ -1,9 +1,13 @@
+const { arch, platform, env } = require('process');
 const path = require('path');
 const { app, nativeImage } = require('electron');
 
 const APP_PATH = app.getAppPath();
 
 module.exports = {
+    ARCH: arch,
+    PLATFORM: platform,
+    ENV: env,
     APP_PATH,
     APP_ICON: nativeImage.createFromPath(path.join(APP_PATH, 'assets/icon.ico')),
 };
