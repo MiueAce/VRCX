c402b155f59d7381fe3c838598c84774e81dc399
pypy <git@pypy.moe>
2021-03-01 21:38:31 +0900

    event-emitter

---
+++ src/js/renderer/event-emitter.js
@@ -0,0 +1,51 @@
+class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+
+    emit(event, ...args) {
+        var handlers = this.events[event];
+        if (typeof handlers === 'undefined') {
+            return;
+        }
+
+        var { length } = handlers;
+        for (var i = 0; i < length; ++i) {
+            handlers[i].apply(this, args);
+        }
+    }
+
+    on(event, handler) {
+        if (typeof handler !== 'function') {
+            return;
+        }
+
+        var handlers = this.events[event];
+        if (typeof handlers !== 'undefined') {
+            handlers.push(handler);
+            return;
+        }
+
+        this.events[event] = [handler];
+    }
+
+    off(event, handler) {
+        var handlers = this.events[event];
+        if (typeof handlers === 'undefined') {
+            return;
+        }
+
+        var index = handlers.indexOf(handler);
+        if (index < 0) {
+            return;
+        }
+
+        handlers.splice(index, 1);
+
+        if (handlers.length === 0) {
+            this.events[event] = undefined;
+        }
+    }
+}
+
+module.exports = EventEmitter;
