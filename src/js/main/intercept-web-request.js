82b2f6cd8690110e4a20f550f12c6077ed5e8a86
pypy <git@pypy.moe>
2021-03-07 01:53:39 +0900

    hide some headers

--- src/js/main/intercept-web-request.js
+++ src/js/main/intercept-web-request.js
@@ -2,36 +2,47 @@ const { WebRequest } = require('electron');
 
 /** @param {WebRequest} webRequest */
 module.exports = function (webRequest) {
-    var filter = {
-        urls: ['https://api.vrchat.cloud/*'],
-    };
+    webRequest.onBeforeSendHeaders(
+        {
+            urls: ['https://api.vrchat.cloud/*', 'wss://pipeline.vrchat.cloud/*'],
+        },
+        function (details, callback) {
+            var { requestHeaders } = details;
 
-    webRequest.onBeforeSendHeaders(filter, function (details, callback) {
-        var { requestHeaders } = details;
+            requestHeaders['Cache-Control'] = 'no-cache';
 
-        requestHeaders['Cache-Control'] = 'no-cache';
+            delete requestHeaders['Sec-Fetch-Site'];
+            delete requestHeaders['Sec-Fetch-Mode'];
+            delete requestHeaders['Sec-Fetch-Dest'];
+            delete requestHeaders['Accept-Language'];
+            delete requestHeaders['Origin'];
 
-        callback({
-            cancel: false,
-            requestHeaders,
-        });
-    });
-
-    webRequest.onHeadersReceived(filter, function (details, callback) {
-        var { responseHeaders } = details;
-
-        if ('set-cookie' in responseHeaders) {
-            var setCookies = responseHeaders['set-cookie'];
-            for (var i = setCookies.length - 1; i >= 0; --i) {
-                setCookies[i] = setCookies[i].replace(/; SameSite=(Strict|Lax)/gi, '') + '; SameSite=None';
-            }
+            callback({
+                cancel: false,
+                requestHeaders,
+            });
         }
+    );
+    webRequest.onHeadersReceived(
+        {
+            urls: ['https://api.vrchat.cloud/*'],
+        },
+        function (details, callback) {
+            var { responseHeaders } = details;
 
-        responseHeaders['access-control-allow-origin'] = ['*'];
+            if ('set-cookie' in responseHeaders) {
+                var setCookies = responseHeaders['set-cookie'];
+                for (var i = setCookies.length - 1; i >= 0; --i) {
+                    setCookies[i] = setCookies[i].replace(/; SameSite=(Strict|Lax)/gi, '') + '; SameSite=None';
+                }
+            }
 
-        callback({
-            cancel: false,
-            responseHeaders,
-        });
-    });
+            responseHeaders['access-control-allow-origin'] = ['*'];
+
+            callback({
+                cancel: false,
+                responseHeaders,
+            });
+        }
+    );
 };
