0f9c6f1c11d44075dad3b8bb8e850ff6a5f40ab3
pypy <git@pypy.moe>
2021-03-07 02:11:36 +0900

    cookie

--- src/js/main/intercept-web-request.js
+++ src/js/main/intercept-web-request.js
@@ -33,7 +33,7 @@ module.exports = function (webRequest) {
             if ('set-cookie' in responseHeaders) {
                 var setCookies = responseHeaders['set-cookie'];
                 for (var i = setCookies.length - 1; i >= 0; --i) {
-                    setCookies[i] = setCookies[i].replace(/; SameSite=(Strict|Lax)/gi, '') + '; SameSite=None';
+                    setCookies[i] = setCookies[i].replace(/; SameSite=(Lax|Strict|None)/gi, '') + '; SameSite=None; Secure';
                 }
             }
 
