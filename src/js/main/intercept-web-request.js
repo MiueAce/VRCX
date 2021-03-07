e88b671ac763100618401d84283ccb6e4e8751c6
pypy <git@pypy.moe>
2021-03-07 18:09:33 +0900

    cleanup

--- src/js/main/intercept-web-request.js
+++ src/js/main/intercept-web-request.js
@@ -1,52 +1,55 @@
 const { WebRequest } = require('electron');
 
-function changeSetCookies(setCookies) {
-    for (var i = setCookies.length - 1; i >= 0; --i) {
-        var cookie = setCookies[i].split(';')[0];
-        setCookies[i] = cookie + '; Path=/; Expires=Thu, 31 Dec 2037 23:55:55 GMT; HttpOnly; Secure; SameSite=None';
+function onWebRequestBeforeSendHeaders(details, callback) {
+    var { requestHeaders } = details;
+
+    requestHeaders['Cache-Control'] = 'no-cache';
+
+    delete requestHeaders['Sec-Fetch-Site'];
+    delete requestHeaders['Sec-Fetch-Mode'];
+    delete requestHeaders['Sec-Fetch-Dest'];
+    delete requestHeaders['Accept-Language'];
+    delete requestHeaders['Origin'];
+
+    callback({
+        cancel: false,
+        requestHeaders,
+    });
+}
+
+function onWebRequestHeadersReceived(details, callback) {
+    var { responseHeaders } = details;
+
+    if ('set-cookie' in responseHeaders) {
+        var headers = responseHeaders['set-cookie'];
+        for (var i = headers.length - 1; i >= 0; --i) {
+            var cookie = headers[i].split(';')[0];
+            headers[i] = cookie + '; Path=/; Expires=Thu, 31 Dec 2037 23:55:55 GMT; HttpOnly; Secure; SameSite=None';
+        }
     }
+
+    responseHeaders['access-control-allow-origin'] = ['*'];
+
+    callback({
+        cancel: false,
+        responseHeaders,
+    });
 }
 
 /** @param {WebRequest} webRequest */
-module.exports = function (webRequest) {
+function interceptWebRequest(webRequest) {
     webRequest.onBeforeSendHeaders(
         {
             urls: ['https://api.vrchat.cloud/*', 'wss://pipeline.vrchat.cloud/*'],
         },
-        function (details, callback) {
-            var { requestHeaders } = details;
-
-            requestHeaders['Cache-Control'] = 'no-cache';
-
-            delete requestHeaders['Sec-Fetch-Site'];
-            delete requestHeaders['Sec-Fetch-Mode'];
-            delete requestHeaders['Sec-Fetch-Dest'];
-            delete requestHeaders['Accept-Language'];
-            delete requestHeaders['Origin'];
-
-            callback({
-                cancel: false,
-                requestHeaders,
-            });
-        }
+        onWebRequestBeforeSendHeaders
     );
     webRequest.onHeadersReceived(
         {
             urls: ['https://api.vrchat.cloud/*'],
         },
-        function (details, callback) {
-            var { responseHeaders } = details;
-
-            if ('set-cookie' in responseHeaders) {
-                changeSetCookies(responseHeaders['set-cookie']);
-            }
-
-            responseHeaders['access-control-allow-origin'] = ['*'];
-
-            callback({
-                cancel: false,
-                responseHeaders,
-            });
-        }
+        onWebRequestHeadersReceived
     );
-};
+}
+
+module.exports = interceptWebRequest;
